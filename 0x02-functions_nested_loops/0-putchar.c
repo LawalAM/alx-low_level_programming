f317535484246615e76f03ff52d63828cde3179e
Lawal Aminat <lawalaminatmotunrayo@gmail.com>
2023-08-15 12:52:20 -0700

    Task0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints _putchar
+ *
+ * Description: prints _putchar
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char text[8] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 9; i++)
+	{
+		putchar(text[i]);
+	}
+	putchar('\n');
+
+	return (0);
+}
